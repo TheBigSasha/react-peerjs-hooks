786f61ac14723c6189f6deb3dc91f3eb5236d060
Sasha Aleshchenko <45843308+TheBigSasha@users.noreply.github.com>
2023-01-08 23:20:51 -0500

    fix tests

--- src/hooks/usePeer/__tests__/index.test.ts
+++ src/hooks/usePeer/__tests__/index.test.ts
@@ -1,14 +1,14 @@
 import { renderHook, act } from '@testing-library/react-hooks';
-import { useJoinPeerSession, useHostPeerSession } from '../index';
+import { useHostMultiPeerSession, useJoinMultiPeerSession } from '../index';
 
 describe('usePeer', () => {
   it('should be defined', () => {
-    expect(useHostPeerSession).toBeDefined();
-    expect(useJoinPeerSession).toBeDefined();
+    expect(useHostMultiPeerSession).toBeDefined();
+    expect(useJoinMultiPeerSession).toBeDefined();
   });
 
   it('should return the correct values', () => {
-    const { result } = renderHook(() => useHostPeerSession({}));
+    const { result } = renderHook(() => useHostMultiPeerSession({}));
     expect(result.current[0]).toBeUndefined();
     expect(result.current[1]).toEqual({});
     expect(result.current[2]).toBeInstanceOf(Function);
@@ -17,7 +17,7 @@ describe('usePeer', () => {
   });
 
   it('should update the state', () => {
-    const { result } = renderHook(() => useHostPeerSession({}));
+    const { result } = renderHook(() => useHostMultiPeerSession({}));
     act(() => {
       result.current[2]({ foo: 'bar' });
     });
